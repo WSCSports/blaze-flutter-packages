ea1fd6aca0ad782238a2a2f7e19050568d1f3215
wmtmobile <mobile-team@wmt.digital>
2025-09-16 20:11:21 +0300

    Remove unsafe bundle force unwrapping

--- blaze_flutter_sdk/ios/Sources/Utils/BlazeFlutterSDKHelper.swift
+++ blaze_flutter_sdk/ios/Sources/Utils/BlazeFlutterSDKHelper.swift
@@ -11,7 +11,7 @@ import BlazeSDK
 class BlazeFlutterSDKHelper: BlazeFlutterSDKHelperProtocol {
     
     lazy var flutterSDKVersion: String? = {
-        let infoDict = Bundle.blazeFlutter.infoDictionary
+        let infoDict = Bundle.blazeFlutter?.infoDictionary
         let flutterVersionName = infoDict?["CFBundleShortVersionString"] as? String
         return flutterVersionName
     }()
@@ -21,11 +21,10 @@ class BlazeFlutterSDKHelper: BlazeFlutterSDKHelperProtocol {
 extension Foundation.Bundle {
     
     /// Since BlazeSDK is a framework, the bundle for classes within this module can be used directly.
-    static let blazeFlutter: Bundle = {
-        let bundleRelativePath = Bundle.main.path(forResource: "blaze-flutter-sdk-bundle",
-                                                  ofType: "bundle")!
-        let bundle = Bundle(path: bundleRelativePath)!
-        return bundle
+    static let blazeFlutter: Bundle? = {
+        Bundle.main
+            .path(forResource: "blaze-flutter-sdk-bundle", ofType: "bundle")
+            .flatMap(Bundle.init(path:))
     }()
     
 }
